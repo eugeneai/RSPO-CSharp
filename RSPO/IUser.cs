00e904dc7d3ec1533ae4353aa4190b445e385d3b
Evgeny Cherkashin <eugeneai@irnok.net>
2018-02-26 16:42:08 +0800

    Added interface for Useer objects.

---
+++ RSPO/IUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BrightstarDB.EntityFramework;
+namespace RSPO
+{
+    [Entity]
+    public interface IUser
+    {
+        string Name { get; set; }
+        string Family { get; set; }
+        string UserName { get; set; }
+        string PasswordHash { get; set; }
+    }
+}
