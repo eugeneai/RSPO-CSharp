6b6c9d4b6e4cc94e6f60b9fd97e43bb2bb9d9c28
Evgeny Cherkashin <eugeneai@irnok.net>
2018-03-20 16:34:38 +0800

    Added classes for loading XML.

---
+++ RSPO/ImportFromAtlcomru.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace RSPO
+{
+    public class RCException: Exception
+    {
+        public RCException(string message) : base(message)
+        {
+        }
+    }
+
+    public class RCNoFileSuppliedException: RCException {
+        public RCNoFileSuppliedException(string message): base(message)
+        {
+        }
+    }
+
+    public class RCFormatException: RCException
+    {
+        public RCFormatException(string message) : base(message)
+        {
+        }
+    }
+
+    public class ImportFromAtlcomru
+    {
+        public string FileName { get; set; } = null;
+        public XDocument document {
+            get
+            {
+                if (_document == null)
+                {
+                    LoadDocument(); // Must set _document to nonNull value or raise an Exception.
+                }
+                return _document;
+            }
+        }
+
+        private void LoadDocument()
+        {
+            if (FileName == null)
+            {
+                throw new RCNoFileSuppliedException("no file supplied for import");
+            }
+            Stream stream = null;
+            if (Path.GetExtension(FileName).ToUpper() == ".ZIP")
+            {
+                try
+                {
+                    using (ZipArchive archive = ZipFile.OpenRead(FileName))
+                    {
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            if (Path.GetExtension(entry.FullName).ToUpper() == ".XML")
+                            {
+                                stream = entry.Open();
+                                break;
+                            }
+                        }
+                        if (stream == null)
+                        {
+                            throw new RCFormatException("could not find XML inside archive");
+                        }
+                    }
+                } catch (InvalidDataException) {
+                    // Doing nothing as this in not ZIP archive.
+                }
+                if (stream == null) {
+                    // Open file directly
+                    stream = File.Open(FileName, FileMode.Open);
+                }
+            }
+            XDocument doc = XDocument.Load(stream);
+        }
+
+        private XDocument _document = null;
+    }
+}
