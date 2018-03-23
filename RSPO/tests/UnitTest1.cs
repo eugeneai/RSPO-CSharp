65158acbcb253e512b6dfbe75ab265bdc941e200
Evgeny Cherkashin <eugeneai@irnok.net>
2018-03-23 22:12:42 +0800

    Added tests, including loading XMLs.

---
+++ RSPO/tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RSPO.tests
+{
+    public class UnitTest1
+    {
+        private string basePath;
+
+        public UnitTest1()
+        {
+            basePath = 
+            Path.GetDirectoryName(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))));
+            basePath = basePath.Replace("file:\\","");
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTestThatPasses()
+        {
+            Assert.NotEqual(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        [Theory]
+        [InlineData("all.xml")]
+        [InlineData("all.xml.zip")]
+        public void ImportTest(string importName)
+        {
+            string dataDir = Path.Combine(basePath, "DATA");
+            dataDir = Path.Combine(dataDir, "Import");
+            string fileName = Path.Combine(dataDir, importName);
+            ImportFromAtlcomru import = new ImportFromAtlcomru()
+            {
+                FileName = fileName
+            };
+            import.Import();
+            Assert.True(true);
+        }
+    }
+}
